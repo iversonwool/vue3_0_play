2ed2c63fd0926f7dca4a58dda780fe0ed8c87307
李浩 <haoli@szlanyou.com>
2026-06-21 08:29:22 +0800

    Format LkPlayground and route '/' to Axe
    Apply code formatting and spacing cleanup in LkPlayground.vue (consistent indentation, spaces in template, minor reflow of reactive object). No functional changes in component behavior. Import and register the Axe component in the router and change the root route component from HomeView to Axe so the Axe view is shown at '/'.

--- vue3playground/src/components/LkPlayground.vue
+++ vue3playground/src/components/LkPlayground.vue
@@ -1,52 +1,53 @@
 <script lang="ts">
-import {ref, reactive,watch} from 'vue'
-  export default {
-    emits: ['hello'],
-    setup(props, context) {
-      const msg = ref('hello')
+import { ref, reactive, watch } from 'vue'
+export default {
+  emits: ['hello'],
+  setup(props, context) {
+    const msg = ref('hello')
 
-      function changeMsg() {
-        msg.value = msg.value + ' ' + 'world!'
-      }
-      function sendMessage() {
-        context.emit('hello', 'lee how')
-      }
+    function changeMsg() {
+      msg.value = msg.value + ' ' + 'world!'
+    }
+    function sendMessage() {
+      context.emit('hello', 'lee how')
+    }
 
-      const person = reactive({
-        name: 'John',
-        age: 19,
-        hobby: {
-          h1: {
-            money: 13
-          }
+    const person = reactive({
+      name: 'John',
+      age: 19,
+      hobby: {
+        h1: {
+          money: 13
         }
-      })
-
-      watch(person, (value, oldValue) => {
-        console.log(value, oldValue)
-      }, { deep: false })
-
-      return {
-        msg,
-        changeMsg,
-        sendMessage,
-        person
       }
+    })
+
+
+
+    watch(person, (value, oldValue) => {
+      console.log(value, oldValue)
+    }, { deep: false })
+
+    return {
+      msg,
+      changeMsg,
+      sendMessage,
+      person
     }
   }
+}
 </script>
 
 
 <template>
-  <h1>{{msg}}</h1>
+  <h1>{{ msg }}</h1>
 
   <button @click="changeMsg">change msg</button>
   <button @click="sendMessage">触发自定义事件</button>
-  <button @click="person.name += '~',person.age += 1,person.hobby.h1.money++">
+  <button @click="person.name += '~', person.age += 1, person.hobby.h1.money++">
     change person info
   </button>
-  <hr/>
+  <hr />
   <span>{{ person.name }}</span> - <span>{{ person.age }}</span>
   <span>{{ person.hobby.h1.money }}</span>
 </template>
-
--- vue3playground/src/router/index.ts
+++ vue3playground/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Axe from '@/components/Axe/Axe.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Axe
     },
     {
       path: '/about',
